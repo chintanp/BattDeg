31997315fde2b039178e6a3c5ed16de554142cfa
Shri Lakshmi S Bonageri <shrilakshmisbonageri@Shris-MacBook-Pro-2.local>
2019-03-16 18:55:18 -0700

    Updated files

--- battdeg/battdeg.py
+++ battdeg/battdeg.py
@@ -15,6 +15,9 @@ def date_time_converter(date_time_list):
     and returns a numpy array with date_time in human readable format. 
     """
     
+    if not isinstance(date_time_list, list):
+        raise TypeError("date_time_list should be a list")
+    
     # Empty array to hold the results
     date_time_human = []
     
@@ -24,40 +27,23 @@ def date_time_converter(date_time_list):
     
     return date_time_human 
 
-    
-#@profile
-def PL_samples_file_joiner(data_dir, file_name_format, ignore_file_indices):
+#@profile 
+def get_dict_files(data_dir, file_name_format, ignore_file_indices):
     """
-    This function reads in the data for PL Samples experiment and returns a 
-    nice dataframe with cycles in ascending order. 
+    This function finds all the files at the location of the file name 
+    format as specified and then creates a dictionary after ignoring the 
+    list of file specified
     
     Args: 
         data_dir (string): This is the absolute path to the data directory. 
-        file_name_format (string): Format of the filename, used to deduce other files.
-        ignore_file_indices (list, int): This list of ints tells which to ignore.
+        file_name_format (string): Format of the filename, used to deduce other 
+        files.
+        ignore_file_indices (list, int): This list of ints tells 
+        which to ignore.
         
     Returns:
-        The complete test data in a dataframe with extra column for capacity in Ah.
+        The dictionary with all data from files dataframes. 
     """
-    
-    # Raise an exception if the type of the inputs is not correct
-    if not isinstance(data_dir, str):
-        raise TypeError('data_dir is not of type string')
-    
-    if not isinstance(file_name_format, str):
-        raise TypeError('file_name_format is not of type string')
-  
-    if not isinstance(ignore_file_indices, list):
-        raise TypeError("ignore_file_indices should be a list")
-
-    for i in range(len(ignore_file_indices)):
-        if not isinstance(ignore_file_indices[i], int):
-            raise TypeError("""ignore_file_indices elements should be 
-            of type integer""")
-    
-    if not os.path.exists(join(data_dir, file_name_format)):
-        raise FileNotFoundError("File {} not found in the location {}"
-        .format(file_name_format, data_dir))
     
     # get the list of files in the directory
     onlyfiles = [f for f in listdir(data_dir) if isfile(join(data_dir, f))]
@@ -88,10 +74,51 @@ def PL_samples_file_joiner(data_dir, file_name_format, ignore_file_indices):
     # Remove the ignored dataframes for characterization 
     dict_ord_cycling_data = {k : dict_ordered[k] for k in wanted_keys}
     
-    # Concatenate the dataframes to create the total dataframe
+    return dict_ord_cycling_data
     
+
+def concat_dict_dataframes(dict_ord_cycling_data):
+    """
+    This function takes in a dictionary with ordered keys 
+    and concatenates the dataframes in the values of the 
+    dictionary to create a large dataframe with all the records. 
+    
+    Args: 
+        dict_ord_cycling_data (dict):
+            The dictionary with ordered integer keys and dataframes as values 
+    
+    Returns: 
+        The dataframe after concatenation 
+    
+    """
+    
+    # Raise an exception if the type of the inputs is not correct
+    if not isinstance(dict_ord_cycling_data, dict):
+        raise TypeError('dict_ord_cycling_data is not of type dict')
+    
+    print(dict_ord_cycling_data.keys())
+    for i in dict_ord_cycling_data.keys():
+        # Raise an exception if the type of the keys is not integers
+        #print(type(i))
+        if not isinstance(i, (int, np.int64)):
+            raise TypeError('a key in the dictionary is not an integer')
+
+    for i in dict_ord_cycling_data.values():
+        # Raise an exception if the type of the values is not a dataframe
+        if not isinstance(i, pd.DataFrame):
+            raise TypeError('a value in the dictionary is not a pandas ' +
+                            'dataframe')
+        #print(i.columns)
+        # Raise am exception if the necessary columns are not found in the df
+        if not {'Cycle', 'Charge_Ah', 'Discharge_Ah', 'Time_sec', 'Current_Amp', 
+                'Voltage_Volt'}.issubset(i.columns):
+            raise Exception("the dataframe doesnt have the columns 'Cycle'" +
+                            ", 'Charge_Ah', 'Discharge_Ah', " + 
+                            "'Time_sec', 'Voltage_Volt', 'Current_Amp' ")
+    
+    # Concatenate the dataframes to create the total dataframe
     df_out = None
-    for k in wanted_keys:
+    for k in dict_ord_cycling_data.keys():
         if df_out is None:
             df_next = dict_ord_cycling_data[k]
             df_out = pd.DataFrame(data=None, columns=df_next.columns)
@@ -102,18 +129,32 @@ def PL_samples_file_joiner(data_dir, file_name_format, ignore_file_indices):
             df_next['Time_sec'] = np.array(df_next['Time_sec']) + max(np.array(df_out['Time_sec']))
             df_next['Charge_Ah'] = np.array(df_next['Charge_Ah']) + max(np.array(df_out['Charge_Ah']))
             df_next['Discharge_Ah'] = np.array(df_next['Discharge_Ah']) + max(np.array(df_out['Discharge_Ah']))
-            df_out = pd.concat([df_out, df_next])
-     
-    ####
-    # This has been commented out for performance, as we do not need date_time
-    ####
-    # Convert the Date_Time from matlab datenum to human readable Date_Time
-    # First convert the series into a numpy array 
-    # date_time_matlab = df_out['Date_Time'].tolist()
-
-    # # Apply the conversion to the numpy array
-    # df_out['Date_Time_new'] =  date_time_converter(date_time_matlab)
+            df_out = pd.concat([df_out, df_next])    
     
+    return df_out
+
+
+def get_cycle_capacities(df_out):
+    """ 
+    This function takes the dataframe, creates a new index and then calculates
+    capacities per cycle from cumulative charge and discharge capacities 
+    
+    Args: 
+        df_out (pandas.DataFrame):
+            Concatenated dataframe 
+    
+    Returns:
+        the dataframe with capacities per cycle
+    
+    """
+    
+    # Raise am exception if the necessary columns are not found in the df
+    if not {'Cycle', 'Charge_Ah', 'Discharge_Ah', 'Time_sec', 'Current_Amp', 
+            'Voltage_Volt'}.issubset(df_out.columns):
+        raise Exception("the dataframe doesnt have the columns 'Cycle'" +
+                        ", 'Charge_Ah', 'Discharge_Ah', " + 
+                        "'Time_sec', 'Voltage_Volt', 'Current_Amp' ")
+
     # Reset the index and drop the old index
     df_out_indexed = df_out.reset_index(drop=True)
 
@@ -153,19 +194,61 @@ def PL_samples_file_joiner(data_dir, file_name_format, ignore_file_indices):
     # due to incorrect discharge_Ah values every few cycles. 
     # But the machine learning algorithm should consider these as outliers and 
     # hopefully get over it. We can come back and correct this. 
-    df_out_indexed['capacity_ah'] = charge_ah - discharge_ah
+    df_out_indexed['capacity_ah'] = charge_cycle_ah - discharge_cycle_ah
+    
+    return df_out_indexed
+
+#@profile
+def pl_samples_file_reader(data_dir, file_name_format, ignore_file_indices):
+    """
+    This function reads in the data for PL Samples experiment and returns a 
+    nice dataframe with cycles in ascending order. 
+    
+    Args: 
+        data_dir (string): This is the absolute path to the data directory. 
+        file_name_format (string): Format of the filename, used to deduce other files.
+        ignore_file_indices (list, int): This list of ints tells which to ignore.
+        
+    Returns:
+        The complete test data in a dataframe with extra column for capacity in Ah.
+    """
+    
+    # Raise an exception if the type of the inputs is not correct
+    if not isinstance(data_dir, str):
+        raise TypeError('data_dir is not of type string')
+    
+    if not isinstance(file_name_format, str):
+        raise TypeError('file_name_format is not of type string')
+  
+    if not isinstance(ignore_file_indices, list):
+        raise TypeError("ignore_file_indices should be a list")
+
+    for i in range(len(ignore_file_indices)):
+        if not isinstance(ignore_file_indices[i], int):
+            raise TypeError("""ignore_file_indices elements should be 
+            of type integer""")
+    
+    if not os.path.exists(join(data_dir, file_name_format)):
+        raise FileNotFoundError("File {} not found in the location {}"
+        .format(file_name_format, data_dir))
+    
+    dict_ord_cycling_data = get_dict_files(data_dir, file_name_format, ignore_file_indices)
+    
+    df_out = concat_dict_dataframes(dict_ord_cycling_data)
+     
+    ####
+    # This has been commented out for performance, as we do not need date_time
+    ####
+    # Convert the Date_Time from matlab datenum to human readable Date_Time
+    # First convert the series into a numpy array 
+    # date_time_matlab = df_out['Date_Time'].tolist()
+
+    # # Apply the conversion to the numpy array
+    # df_out['Date_Time_new'] =  date_time_converter(date_time_matlab)
+    
+    # Get the cycle capacities from cumulative capacities
+    df_out_indexed = get_cycle_capacities(df_out)
+    
 
     return df_out_indexed
     
-    
-def PL_samples_capacity_cycles(pl_df):
-    """
-    This function finds the capacity in each cycle from the cumulative capacity 
-    in the original file. 
-    
-    Args:
-    
-    Returns:
-    """
-    
-    return 
