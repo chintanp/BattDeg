d4342ab643f26de85ef9f2b1345e97d7c6b236eb
Guoyao Chen <guoyao@uw.edu>
2019-03-19 21:47:44 -0700

    battdeg.py after pylint and pep8

--- battdeg/battdeg.py
+++ battdeg/battdeg.py
@@ -17,9 +17,6 @@ import pandas as pd
 import numpy as np
 
 from sklearn.model_selection import train_test_split
-# from sklearn.metrics import mean_squared_error
-# from sklearn.preprocessing import MinMaxScaler
-# from sklearn.preprocessing import LabelEncoder
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
